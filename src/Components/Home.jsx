7b5a4dfbb4fb035dad8609231af6b1734f341d36
Claudio Filho <54292133+claudiopjfilho@users.noreply.github.com>
2022-10-19 23:38:03 -0300

    feat

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -5,13 +5,14 @@ import {
   TwitterLogo,
   InstagramLogo,
   RedditLogo,
+  WhatsappLogo,
 } from "phosphor-react";
 
 export function Home() {
   return (
     <div id="#home" className="font-mono  w-full">
       <div classname="mx-auto">
-        <section className="mt-8 mx-auto flex flex-col md:flex-row items-center justify-between xl:max-w-7xl xl:mx-auto max-w-full px-[8%] flex-wrap w-full">
+        <section className="mt-12 mx-auto flex flex-col md:flex-row items-center justify-between xl:max-w-7xl xl:mx-auto max-w-full px-[8%] flex-wrap w-full">
           <div className="">
             <h2 className="mb-3 flex items-center text-base gap-2 text-gray-400">
               <Minus size={32} color="#dddddd" /> Me chamo:
@@ -24,22 +25,42 @@ export function Home() {
               React, TailwindCSS, CSS, JavaScript e ReactNative.
             </p>
             <div className="mt-3 flex items-center gap-4  w-60">
-              <LinkedinLogo
-                size={32}
-                className=" text-gray-300 hover:text-blue-500 hover:animate-pulse duration-1000"
-              />
-              <GithubLogo
-                size={32}
-                className=" text-gray-300 hover:text-violet-600 hover:animate-pulse duration-1000"
-              />
-              <TwitterLogo
-                size={32}
-                className=" text-gray-300 hover:text-cyan-500 hover:animate-pulse duration-1000"
-              />
-              <RedditLogo
-                size={32}
-                className=" text-gray-300 hover:text-orange-500 hover:animate-pulse duration-1000"
-              />
+              <a
+                href="https://www.linkedin.com/in/claudiopjfilho/"
+                target={"_blank"}
+              >
+                <LinkedinLogo
+                  size={32}
+                  className=" text-gray-300 hover:text-blue-500 hover:animate-pulse duration-1000"
+                />
+              </a>
+              <a
+                href="https://www.github.com/claudiopjfilho/"
+                target={"_blank"}
+              >
+                <GithubLogo
+                  size={32}
+                  className=" text-gray-300 hover:text-violet-600 hover:animate-pulse duration-1000"
+                />
+              </a>
+              <a href="https://twitter.com/claudiopjfilho" target={"_blank"}>
+                <TwitterLogo
+                  size={32}
+                  className=" text-gray-300 hover:text-cyan-500 hover:animate-pulse duration-1000"
+                />
+              </a>
+              <a href="https://www.reddit.com/user/Kr4WD" target={"_blank"}>
+                <RedditLogo
+                  size={32}
+                  className=" text-gray-300 hover:text-orange-500 hover:animate-pulse duration-1000"
+                />
+              </a>
+              <a href="https://wa.me/5585998208571" target={"_blank"}>
+                <WhatsappLogo
+                  size={32}
+                  className=" text-gray-300 hover:text-green-500 hover:animate-pulse duration-1000"
+                />
+              </a>
             </div>
           </div>
           <img
--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,12 +1,12 @@
 import { useState } from "react";
 import { List, X } from "phosphor-react";
 import Logo from "../assets/logo.svg";
+
 export function Nav() {
   const [open, setOpen] = useState(false);
-
   return (
     <div>
-      <header className="border-b border-gray-800  py-2">
+      <header className="`border-b border-gray-800  py-2   ${visible ? 'top-0' : ''} `">
         <div className=" my-2 flex items-center justify-between xl:max-w-7xl xl:mx-auto max-w-full px-[8%] flex-wrap w-full">
           <img
             src={Logo}
@@ -32,7 +32,7 @@ export function Nav() {
             <ul className=" mt-6  lg:mt-0 text-base text-gray-400 flex lg:flex-row flex-col gap-2 lg:justify-between ">
               <li>
                 <a
-                  href=""
+                  href="https://wa.me/5585998208571"
                   className="lg:px-5 py-2 transition-all duration-700 hover:text-gray-300 font-semibold"
                 >
                   Home
