ed26d4cec63634a2e124910be617e752f7474b60
Claudio Filho <54292133+claudiopjfilho@users.noreply.github.com>
2022-11-17 23:09:08 -0300

    add cursos

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,18 @@
-import { useState, useEffect } from "react";
 import Cursos from "./components/Cursos";
 import { Home } from "./components/Home";
 import { Nav } from "./components/Nav";
 import { Projects } from "./components/Projects";
-import Repositories from "./components/Repositories";
 import Techs from "./components/Techs";
 
 function App() {
   return (
-    <div className="App ">
+    <div className="App  ">
       <div>
         <Nav />
         <Home />
         <Projects />
         <Techs />
+        <Cursos />
       </div>
     </div>
   );
--- src/components/Cursos.tsx
+++ src/components/Cursos.tsx
@@ -2,28 +2,48 @@ import React from "react";
 
 export default function Cursos() {
   return (
-    <div>
-      <div>
-        <div id="exp">
-          <ul>
-            <li>
-              <h1>Estagiário - Analista de sistemas</h1>
-              <p>setembro/2022</p>
-            </li>
+    <div className="w-full ">
+      <div className="text-zinc-400 text-md md:text-xl flex flex-col ml-12  md:flex-row  justify-around ">
+        <div id="exp" className="">
+          <ul className="mb-6">
+            <h2 className="mb-4 text-3xl md:text-4xl text-indigo-600 font-extrabold font-mono">
+              Experiência
+            </h2>
+            <div className="ml-4 md:ml-6">
+              <li>
+                <h1 className="text-2xl font-bold text-slate-50">
+                  Estagiário - Analista de Sistemas
+                </h1>
+                <p>Início: setembro/2022 - Atualmente</p>
+              </li>
+            </div>
           </ul>
         </div>
-        <div id="Cursos">
+        <div id="cursos">
           <ul>
-            <li>
-              <h1>Análise e desenvolvimento de sistemas</h1>
-              <p>agosto/2021</p>
-            </li>
-            <li>
-              <h1>Finanças</h1>
-            </li>
-            <li>
-              <h1>JavaScript moderno</h1>
-            </li>
+            <h2 className="mb-4  text-3xl md:text-4xl text-indigo-600 font-extrabold font-mono">
+              Cursos
+            </h2>
+            <div className="ml-4 md:ml-6 ">
+              <li className="mb-8">
+                <h1 className="text-2xl font-bold text-slate-50">Wyden</h1>
+                <p>Análise e desenvolvimento de sistemas</p>
+                <p>Início: agosto/2021</p>
+              </li>
+              <li className="mb-8">
+                <h1 className="text-2xl font-bold text-slate-50">
+                  Universidade Federal do Ceará
+                </h1>
+                <p>Finanças</p>
+                <p>01/2017 - 07/2020</p>
+              </li>
+              <li className="mb-8">
+                <h1 className="text-2xl font-bold text-slate-50">Rocketseat</h1>
+                <p>NLW</p>
+                <p>IgniteLab</p>
+                <p>Maratona Explorer</p>
+              </li>
+            </div>
           </ul>
         </div>
       </div>
