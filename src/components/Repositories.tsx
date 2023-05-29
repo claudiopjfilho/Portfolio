f0b2f6e2174440f38aa16729b8559340c0931da8
Claudio Filho <54292133+claudiopjfilho@users.noreply.github.com>
2023-05-28 23:13:13 -0300

    repo

--- src/components/Repositories.tsx
+++ src/components/Repositories.tsx
@@ -1,13 +1,14 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import "tailwindcss/tailwind.css";
+import { GithubLogo } from "phosphor-react";
 
 interface Repository {
   id: number;
   name: string;
   description: string;
   stargazers_count: number;
-
+  html_url: string;
   language: string;
 }
 
@@ -46,22 +47,22 @@ const GitHubRepos: React.FC<GitHubReposProps> = ({ username }) => {
   return (
     <div>
       <div className="container mx-auto mt-8">
-        <div className="flex justify-between mx-6 my-4">
-          <h2 className="ml-4 text-indigo-700 text-2xl  font-bold mb-4">
+        <div className="flex flex-col md:flex-row justify-between mx-6 my-4">
+          <h2 className="ml-4 text-slate-300 text-2xl  font-bold mb-4">
             Meus Repositórios:
           </h2>
-          <div className="flex gap-8">
+          <div className="flex gap-8 justify-between">
             <button
               onClick={() => setCurrentPage(currentPage - 1)}
               disabled={currentPage === 1}
-              className="bg-slate-700 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded "
+              className="w-24 h-12 bg-slate-700 hover:bg-violet-900 text-white font-bold py-2 px-4 rounded duration-700 cursor-pointer"
             >
               Anterior
             </button>
             <button
               onClick={() => setCurrentPage(currentPage + 1)}
               disabled={endIndex >= repos.length}
-              className="bg-indigo-700 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+              className=" h-12 md:w-24 bg-slate-700 hover:bg-violet-900 text-white font-bold py-2 px-4 rounded duration-700 cursor-pointer"
             >
               Próxima
             </button>
@@ -71,13 +72,15 @@ const GitHubRepos: React.FC<GitHubReposProps> = ({ username }) => {
         {displayedRepos.map(repo => (
           <div
             key={repo.id}
-            className="bg-gray-700 hover:bg-gray-800 p-4 mx-4 mb-4 transition duration-500 ease-in-out rounded-xl"
+            className=" font-mono bg-indigo-900 hover:bg-violet-900 p-4 mx-4 mb-4 transition duration-600 ease-in-out rounded-xl"
           >
             <h3 className="text-xl uppercase text-gray-200 font-bold mb-2 transition duration-500 ease-in-out">
               {repo.name}
             </h3>
-            <p className="text-gray-200 mb-2">{repo.description}</p>
-            <p className="text-gray-200">Estrelas: {repo.stargazers_count}</p>
+            <p className=" text-gray-200 mb-2">{repo.description}</p>
+            <p className="text-gray-200 cursor-pointer">
+              <GithubLogo size={28} href={repo.html_url} className="mb-2" />
+            </p>
             <p className="text-gray-200">Linguagem: {repo.language}</p>
           </div>
         ))}
